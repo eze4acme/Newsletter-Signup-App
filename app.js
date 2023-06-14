17fa4280808032a1ffd403888af001fc032970e4
Eze Onyebuchi <eze4acme@gmail.com>
2023-06-14 14:16:47 +0100

    Newsletter App
    Completed the news letter app using nodejs, express modules and body-parser with mailchimp as database for managing the subscription.

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request')
 const app = express();
-const port = process.env.PORT || 5000; // Use 3000 as the default port if PORT environment variable is not set
+const port = process.env.PORT || 5000; 
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json())
 app.post('/signup', (req, res) => {
     const { firstname, lastname, email} = req.body;
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields:{
+                    FNAME: firstname,
+                    LNAME: lastname
+                }
+            }
+        ]
+    }
+    const postData = JSON.stringify(data)
+    const options = {
+        url: 'https://us11.api.mailchimp.com/3.0/lists/89e3c895b9',
+        method:'POST',
+        headers:{
+            Authorization:'auth e0222f68c730f293257a2ba4d9a08589-us11'
+        },
+        body: postData
+    }
 
-    // Check if required fields are missing
-    // if (!firstname || !lastname || !email) {
-    //     res.redirect('/fail.html');
-    //     return;
-    // }
-
-    console.log(firstname)
-    // Handle successful submission here
+   request(options, (err, response, body) =>{
+        if(err){
+            res.json({error: err})
+        }else{
+            if (response.statusCode === 200) {
+                res.sendStatus(200)
+            }else{
+                res.redirect('fail.html')
+            }
+        }
+   })
 });
 
 app.listen(port, () => {
--- public/index.js
+++ public/index.js
@@ -4,6 +4,9 @@ const fname = document.getElementById('fname');
 const lname = document.getElementById('lname');
 const emailEl = document.getElementById('email');
 const error = document.querySelector('.error');
+const inputs = document.querySelectorAll('input');
+const success = document.querySelector('.success')
+const container = document.querySelector('.container')
 
 button.addEventListener('click', submit);
 
@@ -12,25 +15,37 @@ function submit(e) {
     const firstname = fname.value;
     const lastname = lname.value;
     const email = emailEl.value;
+        if (!firstname || !lastname || !email) {
+            error.style.display = 'block'
+            function handleMouseEnter(event) {
+                error.style.display = 'none'
+            }
+            inputs.forEach(input =>{
+                input.addEventListener('mouseenter', handleMouseEnter);
+            })
+
+        }
     if (firstname && lastname && email) {
-        let data = { 
-            firstname: firstname, 
-            lastname: lastname, 
-            email: lastname
-        };
-        // let postData = ;
-        // console.log(postData);
+        let data = { firstname,  lastname, email };
+        let postData = JSON.stringify(data);
         fetch('/signup', {
             method: 'POST',
-            body: JSON.stringify(data),
+            body: postData,
             headers: {
                 'Content-Type': 'application/json'
             }
         })
-        .then(res => res.json())
-        .then(data => console.log(data.firstname))
+        .then(res =>{
+            if (res.ok) {
+                success.classList.add('success-open')
+                container.style.display = 'none'
+                
+            }else{
+                res.json({error: err})
+            }
+        })
     }
-    form.reset();
+    form.reset()
 }
 
 
@@ -44,42 +59,6 @@ function submit(e) {
 
 
 
-// const form = document.querySelector('form');
-// // const inputs = document.querySelectorAll('input');
-// const button = document.querySelector('button');
-// const fname = document.getElementById('fname')
-// const lname = document.getElementById('lname')
-// const emailEl = document.getElementById('email')
-// const error = document.querySelector('.error');
-
-// // Button listening for click event
-// button.addEventListener('click', submit);
-
-// // Submit function logic
-
-// function submit(e) {
-//     e.preventDefault();
-//     const firstname = fname.value;
-//     const lastname = lname.value;
-//     const email = emailEl.value;
-//     if (firstname && lastname && email ) {
-        
-//         // console.log(data);
-//         let data = {firstname, lastname, email};
-//         let postData = JSON.stringify(data);
-//         console.log(postData);
-//         fetch('/signup', {
-//             method: 'POST',
-//             body: postData,
-//             headers:{
-//                 'Content-Type': 'application/json'
-//             }
-//         })
-//         .then(res => res.json())
-//         .then(data => console.log(data))
-//         form.reset()
-//     }
-// }
 
 
 
@@ -96,59 +75,3 @@ function submit(e) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function submit(e) {
-    // e.preventDefault();
-
-    // Check if any input fields are empty
-    // let isEmpty = false;
-    // inputs.forEach(input => {
-    //     if (input.value.trim() === '') {
-    //         isEmpty = true;
-    //         return;
-    //     }
-    // });
-
-    // if (isEmpty) {
-    //     error.style.display = 'block';
-    //     return;
-    // }
-
-//     const formData = new FormData(form);
-//     const data = Object.fromEntries(formData);
-//     console.log(data);
-
-//     let fetchData = {
-//         method: 'POST',
-//         body: JSON.stringify(data),
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     };
-
-//     fetch('/signup', fetchData)
-//         .then(res => {
-//             if (res.ok) {
-//                 // Handle successful response
-//             } else {
-//                 error.style.display = 'block';
-//             }
-//         });
-// }
